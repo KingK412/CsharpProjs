7f352c265e6d963aeb5f6b429733a85b3d549e81
KingK412 <kingshuk.karuri@gmail.com>
2022-09-28 11:16:51 +0200

    FIrst C# project check-in to Git

---
+++ CSharpTests/CSharpTest01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CSharpTest01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            DerivedA c1 = new DerivedA(); 
+        }
+    }
+
+
+    class Base<T>
+    {
+
+    }
+
+    class DerivedA : Base<string>
+    {
+
+    }
+
+    class DerivedB<T, U> : Base<T>
+    {
+
+    }
+}
